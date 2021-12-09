19e4d54c0c4cbe62fba28bf56337dc840887adcb
M.Farhan Ghifari <ghifarifarhan20@gmail.com>
2021-12-09 22:04:48 +0700

    add new algo for case

--- src/StudiKasus/soal1.java
+++ src/StudiKasus/soal1.java
@@ -2,23 +2,45 @@ package StudiKasus;
 
 import StudiKasus.LinkedList;
 
+import java.util.Scanner;
+
 public class soal1 {
 
   // main soal 1
   public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
     LinkedList list = new LinkedList();
-    list.addLast(5);
-    list.addLast(6);
-    list.addLast(7);
-    list.addLast(8);
-    list.addLast(9);
-    list.print();
-    
-    boolean isNotFound = !list.search(100);
-    if(isNotFound) {
-      System.out.println("Tidak ketemu");
-    } else {
-      System.out.println("Ketemu");
+//    list.addLast(5);
+//    list.addLast(6);
+//    list.addLast(7);
+//    list.addLast(8);
+//    list.addLast(9);
+//    list.print();
+//
+//    boolean isNotFound = !list.search(100);
+//    if(isNotFound) {
+//      System.out.println("Tidak ketemu");
+//    } else {
+//      System.out.println("Ketemu");
+//    }
+    System.out.println(" Masukkan nilai : ");
+    int input = userInput.nextInt();
+    while (input!= 999){
+      if (input!= 999){
+        list.addLast(input);
+      }
+      input = userInput.nextInt();
+
     }
+    System.out.println(" Isi List");
+    list.print();
+
+    System.out.println(" ==== FIND NUMBER =====");
+    int cari = userInput.nextInt();
+    boolean cek = list.find(cari);
+    if (cek)
+      System.out.println("ketemu");
+    else
+      System.out.println("tak ketemu");
   }
 }
--- src/StudiKasus/soal2.java
+++ src/StudiKasus/soal2.java
@@ -2,18 +2,36 @@ package StudiKasus;
 
 import StudiKasus.LinkedList;
 
+import java.util.Scanner;
+
 public class soal2 {
 
     public static void main(String[] args) {
-    LinkedList list = new LinkedList();
-    list.insertFirst(1);
-    list.insertFirst(2);
-    list.insertFirst(3);
-    list.insertFirst(4);
-    list.insertFirst(5);
-    list.insertFirst(6);
-    list.print();
+        Scanner userInput = new Scanner(System.in);
+        int idx = -1;
+        LinkedList list = new LinkedList();
+//  list.insertFirst(1);
+//    list.insertFirst(2);
+//    list.insertFirst(3);
+//    list.insertFirst(4);
+//    list.insertFirst(5);
+//    list.insertFirst(6);
+//    list.print();
+        System.out.println(" Masukkan nilai : ");
+        int input = userInput.nextInt();
+        while (input!= 999){
+            if (input!= 999){
+                list.addLast(input);
+            }
+            input = userInput.nextInt();
+        idx ++;
+        }
+        System.out.println(" Isi List");
+        list.print();
     System.out.println("Jumlah seluruh node adalah");
-    System.out.println(list.jumlahSeluruhNode());
+    System.out.println(list.sumOfAllNode(list.getHead()));
+//    System.out.println(list.jumlahSeluruhNode());
+        System.out.println(" rata2 : "+(list.sumOfAllNode(list.getHead()))/(idx+1));
   }
+
 }
