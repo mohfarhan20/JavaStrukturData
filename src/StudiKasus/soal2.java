739289b0a0aa70a6235e44e78ac910e63836c7ad
M.Farhan Ghifari <ghifarifarhan20@gmail.com>
2021-12-10 10:41:59 +0700

    update main

--- src/StudiKasus/soal2.java
+++ src/StudiKasus/soal2.java
@@ -10,13 +10,7 @@ public class soal2 {
         Scanner userInput = new Scanner(System.in);
         int idx = -1;
         LinkedList list = new LinkedList();
-//  list.insertFirst(1);
-//    list.insertFirst(2);
-//    list.insertFirst(3);
-//    list.insertFirst(4);
-//    list.insertFirst(5);
-//    list.insertFirst(6);
-//    list.print();
+
         System.out.println(" Masukkan nilai : ");
         int input = userInput.nextInt();
         while (input!= 999){
@@ -32,6 +26,14 @@ public class soal2 {
     System.out.println(list.sumOfAllNode(list.getHead()));
 //    System.out.println(list.jumlahSeluruhNode());
         System.out.println(" rata2 : "+(list.sumOfAllNode(list.getHead()))/(idx+1));
+
+        //  list.insertFirst(1);
+//    list.insertFirst(2);
+//    list.insertFirst(3);
+//    list.insertFirst(4);
+//    list.insertFirst(5);
+//    list.insertFirst(6);
+//    list.print();
   }
 
 }
