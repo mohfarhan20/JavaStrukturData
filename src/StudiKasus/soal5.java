1e89d0ceab460103f4ee148aa4e0d152145d1dbf
M.Farhan Ghifari <ghifarifarhan20@gmail.com>
2021-12-10 10:41:28 +0700

    update main

--- src/StudiKasus/soal5.java
+++ src/StudiKasus/soal5.java
@@ -1,19 +1,21 @@
 package StudiKasus;
 
-import StudiKasus.BinarySearchTree;
+import java.util.*;
 
 public class soal5 {
 
   public static void main(String[] args) {
     BinarySearchTree bst = new BinarySearchTree();
-    bst.insert(1);
-    bst.insert(2);
-    bst.insert(3);
-    bst.insert(4);
-    bst.insert(5);
-    bst.insert(6);
-
-
+   Scanner sc = new Scanner(System.in);
+    System.out.println(" Masukkan data genap : ");
+    int value = sc.nextInt();
+    while (value != 999){
+      if (value!= 999)
+      bst.insert(value);
+     value = sc.nextInt();
+    }
     System.out.println(bst);
+
+
   }
 }
